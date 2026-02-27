c094f1536f376200c327fd01c0d324403656c7ef
skibiditweaks <pajeroporrero@gmail.com>
2026-02-27 16:54:32 +0100

    Update sourcetest.lua

--- DELETE-AFTER/sourcetest.lua
+++ DELETE-AFTER/sourcetest.lua
@@ -462,8 +462,48 @@ CreateButton("Escape Getting Crushed For Brainrots", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/skibiditweaks/wethub/refs/heads/main/hubparts/escapegettingcrushed.lua"))()
 end)
 
-CreateButton("⛔ BrookHaven (NOT MINE, MIGHT NOT WORK)", function()
+CreateButton("BrookHaven (NOT MINE, MIGHT NOT WORK)", function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Cat558-uz/All-rights-reserved-to-packj0/main/hub%20Brook"))()
+end)
+
+CreateButton("Adopt Me (Fly & Ride Pets)", function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/skibiditweaks/wethub/refs/heads/main/hubparts/adoptmee.lua"))()
+end)
+
+CreateButton("Anti Aimbot (KEYBIND Z) (PC ONLY)", function()
+local Toggled = true
+local KeyCode = 'z'
+local hip = 2.80
+local val = -35
+
+
+
+
+
+function AA()
+    local oldVelocity = game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity
+    game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(oldVelocity.X, val, oldVelocity.Z)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(oldVelocity.X, oldVelocity.Y, oldVelocity.Z)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(oldVelocity.X, val, oldVelocity.Z)
+    game.Players.LocalPlayer.Character.Humanoid.HipHeight = hip
+end
+
+game:GetService('UserInputService').InputBegan:Connect(function(Key)
+    if Key.KeyCode == Enum.KeyCode[KeyCode:upper()] and not game:GetService('UserInputService'):GetFocusedTextBox() then
+        if Toggled then
+            Toggled = false
+            game.Players.LocalPlayer.Character.Humanoid.HipHeight = hip
+
+        elseif not Toggled then
+            Toggled = true
+
+            while Toggled do
+                AA()
+                task.wait()
+            end
+        end
+    end
+end)
 end)
 
 -- INITIALIZE LOAD
